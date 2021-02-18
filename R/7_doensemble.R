8013e4538c045e5ff1ee3808da0f80c659483b2a
Carlos Munoz <cmunoz@humboldt.org.co>
2021-02-18 16:01:39 -0500

    reorganizing ensembles by algorithm

--- R/7_doensemble.R
+++ R/7_doensemble.R
@@ -1,108 +1,191 @@
 do.ensemble <- function(respathA, respathB1, respathB2, do.future,
                         occ., threshold., col.lon, col.lat, folder.sp,
-                        crs.proyect){
-  
-  # empty raster to save layers from modeling process 
-  ras_current <- raster::stack()
-  
-  # results from small samples maxent modeling
-  if(!is.null(respathA)){
-    ras_current <-  raster::stack(respathA$c_proj, ras_current)
+                        crs.proyect) {
+
+  # ensemble from small samples maxent modeling if exist
+  if (!is.null(respathA)) {
+    pathA_current <- respathA$c_proj
+    raster::crs(pathA_current) <- sp::CRS(crs.proyect)
+    ensA <- currentEns_byAlg(
+      ras.Stack = pathA_current, data. = occ., collon = col.lon, collat = col.lat,
+      e = 10, algorithm = "mxnt", foldersp = folder.sp # MISSING let user choice e
+    ) ########################
   }
-  
-  # results from large samples maxent modeling
-  if(!is.null(respathB1)){
-    ras_current <- raster::stack(respathB1$c_proj, ras_current)
+
+  # ensemble from large samples maxent modeling  if exist
+  if (!is.null(respathB1)) {
+    pathB1_current <- respathB1$c_proj
+    raster::crs(pathB1_current) <- sp::CRS(crs.proyect)
+    ensB1 <- currentEns_byAlg(
+      ras.Stack = pathB1_current, data. = occ., collon = col.lon, collat = col.lat,
+      e = 5., algorithm = "mxnt", foldersp = folder.sp # MISSING let user choice e
+    )
   }
-  
-  # results from small samples other modeling
-  if(!is.null(respathB2)){
-    ras_biomod01 <- (respathB2$c_proj)/1000
-    ras_current <- raster::stack(ras_biomod01, ras_current)
-  }
-  
-    raster::crs(ras_current) <- sp::CRS(crs.proyect)
-  # MISSING only one model
-  
-  # branch for species with only one best model
-  if(raster::nlayers(ras_current) < 1){
+
+  # ensemble from large samples other algorithms modeling  if exist
+  if (!is.null(respathB2)) {
+    pathB2_current <- (respathB2$c_proj)
+    raster::crs(pathB2_current) <- sp::CRS(crs.proyect)
     
-    stop("Number of models is one (1). Cannot compute ensembles")
+    # MISSING let the user specify the algorithms used in biomod, it could be solve using a
+    # flag name of maxent (mxnt) in the raster names (in kuenm or enmeval) to change the ensemble algorithm to
+    # use the name to know what algorith was used
+    # Temporal solution:
+
+    pathB2GBM <- pathB2_current[[grep(pattern = "GBM", names(pathB2_current))]]
+    ensB2GBM <- currentEns_byAlg(
+      ras.Stack = pathB2GBM, data. = occ., collon = col.lon, collat = col.lat,
+      e = 5, algorithm = "GBM", foldersp = folder.sp ############ MISSING let user choice e
+    )
+
+    pathB2ANN <- pathB2_current[[grep(pattern = "ANN", names(pathB2_current))]]
+    ensB2ANN <- currentEns_byAlg(
+      ras.Stack = pathB2ANN, data. = occ., collon = col.lon, collat = col.lat,
+      e = 5, algorithm = "ANN", foldersp = folder.sp ############ MISSING let user choice e
+    )
+  }
+
+  # if(do.future == T){
+  #
+  # }
+}
+
+#--------------------------
+
+currentEns_byAlg <- function(ras.Stack, data., collon, collat, e, algorithm, foldersp) {
   
-  }else if(raster::nlayers(ras_current) > 1){
-  
-  # branch for species with more than one best model
-  
-  # extracting values for all occurrences from models and organize in a data.frame  
-  rasvalue <- raster::extract(ras_current, occ.[ , c(col.lon, col.lat)]) %>%
-    na.omit() %>% apply(., MARGIN = 2, sort) %>% as.data.frame()
-  
-  # percentile data of E
-  EData <- ceiling((nrow(occ.) * threshold.) / 100)
-  
-  # value for each model to binarize to E
-  EValue <- rasvalue[EData, ]
+  if (is.null(ras.Stack)) {
+    stop("any model")
+  }
   
   # folder to save ensembles
   dir.create(paste0(folder.sp, "/ensembles"), showWarnings = FALSE)
   dir.create(paste0(folder.sp, "/ensembles/current"), showWarnings = F)
+  dir.create(paste0(folder.sp, "/ensembles/current", "/", algorithm), showWarnings = F)
+  # branch for algorithms with more than one best model
 
-  # writing data.frame of E values to convert to binary with E
-  write.csv(EValue, paste0(folder.sp, "/ensembles/current/EValue.csv"), row.names = F)
-  
-  ## Converting continuous prediction to binary according to bin_threshold
-  
-  binE <- raster::stack()
-  for(i in 1:raster::nlayers(ras_current)){
-    EValue_layer <- EValue[ , i]
-    bin_layer <- ras_current[[i]] > EValue_layer
-    binE <- raster::stack(binE, bin_layer)
+  if (nlayers(ras.Stack) > 1) {
+
+    # 1. Logistic ensembles by algorithm
+
+    # taking statistics from logistic stack
+    Ras.med <- raster::calc(x = ras.Stack, median)
+    Ras.devstd <- raster::calc(x = ras.Stack, sd)
+    Ras.cv <- raster::calc(x = ras.Stack, CV)
+    Ras.sum <- raster::calc(x = ras.Stack, sum)
+
+    # stacking results of ensembles
+    Resensembles <- stack(Ras.med, Ras.devstd, Ras.cv, Ras.sum)
+    names(Resensembles) <- c(
+      paste0(folder.sp, algorithm, "_med"), paste0(folder.sp, algorithm, "_devstd"),
+      paste0(folder.sp, algorithm, "_CV"), paste0(folder.sp, algorithm, "_sum")
+    )
+
+    # writing ensemble
+    for (i in 1:raster::nlayers(Resensembles)) {
+      writeRaster(Resensembles[[i]], paste0(
+        folder.sp, "/ensembles/current", "/", algorithm, "/",
+        names(Resensembles[[i]]), ".tif"
+      ),
+      format = "GTiff",
+      overwrite = T
+      )
+    }
   }
-  
-  names(binE) <- names(ras_current)
-  
-  dir.create(paste0(folder.sp, "/ensembles/current/binariesE"), showWarnings = F)
-  for(i in 1:raster::nlayers(binE)){
-    writeRaster(binE[[i]], paste0(
-      folder.sp, "/ensembles/current/binariesE/",
-      names(binE[[i]]), "binE.tif"
+
+  if (nlayers(ras.Stack) == 1) {
+    # pseudo-result of ensemble 1 model (median), to conserve parsimony
+    Ras.med <- ras.Stack
+    names(Ras.med) <- paste0(folder.sp, algorithm, "_med")
+    # writing pseudo median
+    writeRaster(Ras.med, paste0(
+      folder.sp, "/ensembles/current", "/", algorithm, "/",
+      names(Ras.med), ".tif"
+    ))
+  }
+
+  # thresholds
+
+  biomodelos.thresh <- c(e, 1, 10, 20, 30)
+  names(biomodelos.thresh) <- c("E", "MTP", "TTP", "20TP", "30TP")
+
+  # converting to binary the median ensemble for each biomodelos threshold
+
+  Bins <- lapply(X = biomodelos.thresh, function(X) {
+    do.bin(
+      Ras = Ras.med, dat = data., lon = collon,
+      lat = collat, thresh = X
+    )
+  })
+
+  # extracting threshold value from list of binaries. Bins file is a list o sub-list. Each sub-list
+  # refers to one threshold conversion. Inside each sub-list there are two files, the binary raster
+  # and the value of threshold
+  BinsDf <- list()
+  for (i in 1:length(Bins)) {
+    listi <- Bins[[i]]
+    Df <- listi[[2]]
+    BinsDf[[i]] <- Df
+  }
+  # convert list to a vector
+  BinsDf <- data.frame(BinsDf)
+  # giving names to the Df
+  names(BinsDf) <- names(Bins)
+
+  write.csv(BinsDf, paste0(
+    folder.sp, "/ensembles/current/", algorithm, "/",
+    "binValues.csv"
+  ),
+  row.names = F
+  )
+
+  #
+  BinsRas <- raster::stack()
+  for (i in 1:length(Bins)) {
+    listi <- Bins[[i]]
+    Ras <- listi[[1]]
+    BinsRas <- raster::stack(BinsRas, Ras)
+  }
+
+  names(BinsRas) <- paste0(foldersp, algorithm, "_Bin", names(Bins))
+
+  for (i in 1:nlayers(BinsRas)) {
+    writeRaster(BinsRas[[i]], paste0(
+      folder.sp, "/ensembles/current/", algorithm, "/",
+      names(BinsRas[[i]]), ".tif"
     ),
     format = "GTiff",
     overwrite = T
     )
   }
-  
-  # variation coeficient function
-  CV <- function(x, na.rm = TRUE) {
-    sd(x, na.rm=na.rm) / mean(x, na.rm=na.rm)
-  }
-  
-  # taking statistics from binary stack
-  binE_med <- raster::calc(x = binE, median)
-  binE_devstd <- raster::calc(x = binE, sd)
-  binE_cv <- raster::calc(x = binE, CV)
-  binE_sum <- raster::calc(x =binE, sum)/nlayers(binE)
-  
-  # stacking results of ensembles
-  Resensembles <- stack(binE_med, binE_devstd, binE_cv, binE_sum)
-  names(Resensembles) <- c(paste0(folder.sp,"_mediana"), paste0(folder.sp,"_devstd"), 
-                           paste0(folder.sp,"_CV"), paste0(folder.sp,"_sumNorm"))
-  # writing ensembles
-  for(i in 1:raster::nlayers(Resensembles)){
-    writeRaster(Resensembles[[i]], paste0(
-      folder.sp, "/ensembles/current/",
-      names(Resensembles[[i]]), ".tif"
-    ),
-    format = "GTiff",
-    overwrite = T
-    )
-  }
-  
-  }
-  
-  #if(do.future == T){
-  #  
-  #}
-     
 }
 
+#------------------------
+# variation coeficient function
+CV <- function(x, na.rm = TRUE) {
+  sd(x, na.rm = na.rm) / mean(x, na.rm = na.rm)
+}
+
+#------------------------------
+
+do.bin <- function(Ras, dat, lon, lat, thresh) {
+
+  # extracting values for all occurrences from models and organize in a data.frame
+  rasvalueT <- raster::extract(Ras, dat[, c(lon, lat)]) %>%
+    na.omit() %>%
+    sort()
+
+  # percentile data of E
+  TData <- ceiling((nrow(dat) * thresh) / 100)
+
+  # value for each model to binarize to E
+  TValue <- rasvalue[TData]
+  names(TValue) <- names(thresh)
+
+  ## Converting continuous prediction to binary according to bin_threshold
+
+  binT <- Ras.med > TValue
+  names(binT) <- names(thresh)
+
+  return(list(binT, TValue))
+}
