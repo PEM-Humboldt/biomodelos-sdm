5a443da4e8ca3557a8478c56f5d77fdf1baa4755
Carlos Munoz <cmunoz@humboldt.org.co>
2021-02-19 08:34:58 -0500

    deal with ID column inside occurrences DF

--- R/6B_1_dosplit.R
+++ R/6B_1_dosplit.R
@@ -28,7 +28,7 @@ dosplit <- function(occ., bias.file, folder.sp, col.lon, col.lat) {
     ]
   }
   
-  # enmeval needs a data frame with longitude and latitude coordinates with these names and this
+  # enmeval way to split needs a data frame with longitude and latitude coordinates with these names and this
   # order
   data. <- occ.[, c(col.lon, col.lat)]
   names(data.) <- c("longitude", "latitude")
@@ -40,19 +40,45 @@ dosplit <- function(occ., bias.file, folder.sp, col.lon, col.lat) {
   
   # Assigning occurrences to train and test by groups
   
-  occ_train <- dplyr::filter(bins.data, bins == 2 |bins == 4| bins == 1)[, c(1,2)]
+  occ_train <- dplyr::filter(bins.data, bins == 2 |bins == 4| bins == 1)[, c("longitude","latitude")]
 
-  occ_test <- dplyr::filter(bins.data, bins == 3)[, c(1,2)]
+  occ_test <- dplyr::filter(bins.data, bins == 3)[, c("longitude","latitude")]
 
-  occ_joint <- rbind(occ_train, occ_test)[, c(1,2)]
+  occ_joint <- rbind(occ_train, occ_test)[, c("longitude","latitude")]
 
   # writing occurrence data
 
-  write.csv(x = occ_train, file = paste0(folder.sp, "/occurrences/occ_train.csv"), row.names = F)
+  write.csv(x = occ_train, file = paste0(folder.sp, "/occurrences/occ_train_biomod.csv"), row.names = F)
 
-  write.csv(x = occ_test, file = paste0(folder.sp, "/occurrences/occ_test.csv"), row.names = F)
+  write.csv(x = occ_test, file = paste0(folder.sp, "/occurrences/occ_test_biomod.csv"), row.names = F)
 
-  write.csv(x = occ_joint, file = paste0(folder.sp, "/occurrences/occ_joint.csv"), row.names = F)
+  write.csv(x = occ_joint, file = paste0(folder.sp, "/occurrences/occ_joint_biomod.csv"), row.names = F)
 
   return(list(occ_train = occ_train, occ_test = occ_test, occ_joint = occ_joint))
+  
+  #------------------------------------
+  # tracking occurrences with id
+  
+  lonlatTrain <- paste0(occ_train$longitude, occ_train$latitude)
+  lonlatTest <- paste0(occ_test$longitude, occ_test$latitude)
+  lonlatocc <- paste0(occ.$decimalLongitude, occ.$decimalLatitude)
+  
+  indexTrain <- as.numeric()
+  for(i in 1:length(lonlatTrain)){
+    val1 <- which(lonlatTrain[i] == lonlatocc)
+    indexTrain[i] <- val1
+  }
+  occ_train_id <- occ.[indexTrain, ]
+  
+  indexTest <- as.numeric()
+  for(i in 1:length(lonlatTest)){
+    val1 <- which(lonlatTest[i] == lonlatocc)
+    indexTest[i] <- val1
+  } 
+  occ_test_id <- occ.[indexTest, ]
+  
+  # writing tracking files with id
+  write.csv(x = occ_train_id, file = paste0(folder.sp, "/occurrences/occ_trainID.csv"), row.names = F)
+  write.csv(x = occ_test_id, file = paste0(folder.sp, "/occurrences/occ_testID.csv"), row.names = F)
+  
 }
