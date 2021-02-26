7a95df7dd58da85ef496699bef77839b073a6578
Carlos Munoz <cmunoz@humboldt.org.co>
2021-02-26 11:53:59 -0500

    paralel process by species

--- paralel_process.R
+++ paralel_process.R
@@ -2,64 +2,64 @@
 
 source("setup.R")
 
-do.load(vector.packages)
+#do.load(vector.packages)
 
-if (!require(kuenm)) {
-  devtools::install_github("marlonecobos/kuenm")
-}
+#if (!require(kuenm)) {
+#  devtools::install_github("marlonecobos/kuenm")
+#}
 
 # Set 16 Primates (test)
 
-# csv
+set_aves <- read.csv("AgendaModelado/aves/AM_aves_240221_mongo.csv", stringsAsFactors = F, sep = ",")
+list_aves <- split(set_aves, f = set_aves$acceptedNameUsage)
 
 # rutina
 source("R/Bio2_routine.R")
+
+# parallel
 
 # initializing
 
 library(parallel)
 
+# vector.packages
+# [1] "plyr"              "dplyr"             "automap"           "PresenceAbsence"   "devtools"
+# [6] "CoordinateCleaner" "sf"                "spThin"            "raster"            "dismo"
+# [11] "biomod2"           "ENMeval"           "rgdal"
+# plus
+# "kuenm"
 
-for(i in 1:length(a)){
-  closeAllConnections()
-  i2 <- a[i]
-  Bio2_routine(occ = occ_list[[i2]],
-               drop_out = "any",
-               polygon_M = "Data/biogeographic_shp/bior_colextent/bior.shp",
-               raster_M = NULL,
-               proj_models = "M-M",
-               dist_MOV = 200, 
-               do_future = FALSE,
-               clim_vars = "worldclim",
-               dir_clim = "Data/env_vars/",
-               dir_other = "Data/env_vars/other/",
-               TGS_kernel = "bias_layer/primates.tif"
-              )
-}
-
-
-####
-
-# parallel
-
-
-
-cl <- parallel::makeCluster(detectCores())
-clusterEvalQ(cl, {library(dplyr, logical.return = T)
-  library(ggplot2, logical.return = T)
-  library(sf, logical.return = T)
-  library(rnaturalearth, logical.return = T)
-  library(rnaturalearthdata, logical.return = T)
-  library(ggspatial, logical.return = T)
-  library(raster, logical.return = T)
+cl <- parallel::makeCluster(detectCores() - 2)
+clusterEvalQ(cl, {
+  library(plyr, logical.return = T)
+  library(dplyr, logical.return = T)
+  library(PresenceAbsence, logical.return = T)
   library(CoordinateCleaner, logical.return = T)
-  library(sqldf, logical.return = T)
-  library(automap, logical.return = T)
+  library(sf, logical.return = T)
+  library(spThin, logical.return = T)
+  library(raster, logical.return = T)
   library(dismo, logical.return = T)
   library(biomod2, logical.return = T)
+  library(ENMeval, logical.return = T)
+  library(rgdal, logical.return = T)
   library(kuenm, logical.return = T)
-  library(PresenceAbscence, logical.return = T)
 })
-parallel::parLapply(cl = cl, X = occ_list, fun = rutina16k)
+clusterExport(cl = cl, list("Bio2_routine"),
+              envir=environment())
+parallel::parLapply(cl = cl, X = list_aves, function(X) {
+  Bio2_routine(
+    occ = X,
+    drop_out = "any",
+    polygon_M = "Data/biogeographic_shp/wwf_ecoregions/wwf_terr_ecos.shp",
+    raster_M = NULL,
+    proj_models = "M-M",
+    dist_MOV = 222,
+    do_future = FALSE,
+    clim_vars = "worldclim",
+    dir_clim = "Data/env_vars/",
+    dir_other = "Data/env_vars/other/",
+    TGS_kernel = "bias_layer/aves.tif"
+  )
+})
 stopCluster(cl)
 
